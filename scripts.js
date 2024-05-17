ac2de2c4ceb38cbaeeb65385e5defb0c762ee186
Phillip Bogopane <pm.bogopane@gamil.com>
2024-05-17 16:22:17 +0200

    Removed another left over code that was not working with code I coded.

--- scripts.js
+++ scripts.js
@@ -26,20 +26,6 @@ class BookList {
         this.updateButtonLabel();
     }
     
-document.querySelector('[data-list-items]').appendChild(starting)
-
-const genreHtml = document.createDocumentFragment()
-const firstGenreElement = document.createElement('option')
-firstGenreElement.value = 'any'
-firstGenreElement.innerText = 'All Genres'
-genreHtml.appendChild(firstGenreElement)
-
-for (const [id, name] of Object.entries(genres)) {
-    const element = document.createElement('option')
-    element.value = id
-    element.innerText = name
-    genreHtml.appendChild(element)
-}
 
 render() {
     const fragment = document.createDocumentFragment();
