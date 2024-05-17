9994fcd4aa90115af6a6208633b7a27dabe5fd7e
Phillip Bogopane <pm.bogopane@gamil.com>
2024-05-17 15:43:56 +0200

    Coded code that is reponsible for displaying detailed information about a book when user interacts with book preview element.

--- scripts.js
+++ scripts.js
@@ -163,7 +163,22 @@ addEventListeners() {
                 active = result;
             }
         }
-        
+
+        if (active) {
+            document.querySelector('[data-list-active]').open = true;
+            document.querySelector('[data-list-blur]').src = active.image;
+            document.querySelector('[data-list-image]').src = active.image;
+            document.querySelector('[data-list-title]').innerText = active.title;
+            document.querySelector('[data-list-subtitle]').innerText = `${authors[active.author]} (${new Date(active.published).getFullYear()})`;
+            document.querySelector('[data-list-description]').innerText = active.description;
+        }
+    });
+}
+
+
+
+
+
     document.querySelector('[data-list-items]').appendChild(newItems)
     document.querySelector('[data-list-button]').disabled = (matches.length - (page * BOOKS_PER_PAGE)) < 1
 
